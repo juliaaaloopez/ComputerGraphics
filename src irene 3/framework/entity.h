41d6677b0d2649816b551e54e8e86f9623151502
Ireneee01 <131949284+Ireneee01@users.noreply.github.com>
2024-02-02 16:28:44 +0100

    Add files via upload

---
+++ src irene 3/framework/entity.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include "mesh.h"
+#include "image.h"
+
+class Entity {
+
+public:
+
+    Mesh* mesh;
+    Matrix44 model;
+
+    Entity() {
+        this->mesh = new Mesh;
+        this->model = Matrix44(); //Sets identity matrix
+    };
+
+    ~Entity() {};
+
+    void Render(Image* framebuffer, Camera* camera, const Color& c);
+    void Update(float seconds_elapsed, bool rotate, bool translate, bool scale);
+
+    enum class eRenderMode {
+        POINTCLOUD,
+        WIREFRAME,
+        TRIANGLES,
+        TRIANGLES_INTERPOLATED
+    };
+    eRenderMode mode;
+
+};
